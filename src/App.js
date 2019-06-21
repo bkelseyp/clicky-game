74416c0f5b526f910b3320e9c3b8d73206624973
Brianna Kelsey <briannagkelsey@gmail.com>
2019-06-20 23:44:55 -0400

    edited app.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Characters from "./caracters.json";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+        </Wrapper>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Wrapper>
+//           <Route exact path="/" component={Home} />
+//         </Wrapper>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer text-center pt-4 pb-4">
+      <span className="pt-2 pb-2"> Character Clicky Game</span>
+    </footer>
+  );
+}
+
+export default Footer;
